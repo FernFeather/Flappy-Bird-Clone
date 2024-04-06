fac36fb2e6e6fd716b843cd357e4de2592d9a905
Juliana Lucas <63823731+FernFeather@users.noreply.github.com>
2024-04-06 23:55:20 +0000

    create player floor and obstacles

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+let player, floor, obstacles;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
+
+  //Create floor object
+  floor = new Sprite(150, 580, 800, 40, "n");
+
+  //Create player object
+  Player = new Sprite(0, height / 2);
+
+  //Create game obstacles group
+  obstacles = new Group();
+  obstacles.h = 400;
+  obstacles.w = 80;
+  obstacles.color = "orange";
+  obstacles.collider = "s";
 }
 
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  noStroke();
 }
